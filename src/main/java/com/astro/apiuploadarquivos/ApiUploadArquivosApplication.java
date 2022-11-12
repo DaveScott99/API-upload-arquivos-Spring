e0ef132a7c08d5d9bb7411ae32e0d01a8c9cebed
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-11-12 11:13:55 -0300

    Upload de arquivos

---
+++ src/main/java/com/astro/apiuploadarquivos/ApiUploadArquivosApplication.java
@@ -0,0 +1,13 @@
+package com.astro.apiuploadarquivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUploadArquivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUploadArquivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astro/apiuploadarquivos/ApiUploadArquivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.apiuploadarquivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUploadArquivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
